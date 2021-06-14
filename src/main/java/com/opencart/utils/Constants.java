c05c9d2a5cda2c7b27538b6385c51ac45ae7e776
raksha.thakre <raksha.thakre@empower-solutions.co.in>
2021-06-14 09:23:08 -0400

    updated the constants file

--- src/main/java/com/opencart/utils/Constants.java
+++ src/main/java/com/opencart/utils/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 	
 	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String ACCOUNTS_PAGE_TITLE = "My Account";
-	public static final String ACCOUNTS_PAGE_HEADER = "Your Store1";
+	public static final String ACCOUNTS_PAGE_HEADER = "Your Store";
 	public static final List<String> ACC_SEC_LIST 
 			= Arrays.asList("My Account", 
 				"My Orders", 
